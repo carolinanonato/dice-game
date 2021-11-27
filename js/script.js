7164a764298abb487927d749b959684fa1bb756b
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-27 18:01:48 -0500

    dice faces are now showing when refreshing

--- js/script.js
+++ js/script.js
@@ -7,12 +7,44 @@ const getRandomDiceRoll = function(sides=6) {
 }
 
 // Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
 
 // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
+let diceRoll = getRandomDiceRoll();
+
+console.log(diceRoll);
 
 // 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
 
+image = document.querySelector("img");
+
+if (diceRoll === 1) {
+  document.querySelector('img').src="img/dice1.svg"
+}
+
+if (diceRoll === 2) {
+  document.querySelector('img').src="img/dice2.svg"
+}
+
+if (diceRoll === 3) {
+  document.querySelector('img').src="img/dice3.svg"
+}
+
+if (diceRoll === 4) {
+  document.querySelector('img').src="img/dice4.svg"
+}
+
+if (diceRoll === 5) {
+  document.querySelector('img').src="img/dice5.svg"
+}
+
+if (diceRoll === 6) {
+  document.querySelector('img').src="img/dice6.svg"
+}
+
+
 // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
+
+
+
 
 // 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
