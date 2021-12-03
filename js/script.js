04089a7d58eb77d7081266162f56bd3d0d392d40
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-12-02 21:21:12 -0500

    craps game done

--- js/script.js
+++ js/script.js
@@ -1,82 +1,49 @@
-// This function returns a random Number between 1 (inclusive) and the argument passed to the function
-// If no argument is passed, 6 is set, ie, returns a number between and including 1 to 6
-// 		For example: 
-//    getRandomDiceRoll() will return either 1, 2, 3, 4, 5, 6
 const getRandomDiceRoll = function(sides=6) {
   return Math.floor( Math.random() * sides ) + 1
 }
 
-// Keep track of the previous roll in the global scope so we always remember what we rolled the last time
-let lastRoll = 0
+ let lastRoll=0
+ var str=" again!"
+ const rollTheDice=function () {
+ let diceRoll1=getRandomDiceRoll();
+ let diceRoll2=getRandomDiceRoll();
+ let sum =diceRoll1+diceRoll2
 
-
-const rollTheDice = function() {
-  
-  // Random number between 1 and 6 (inclusive)
-  const rollNum = getRandomDiceRoll()
-
-
-  //////////////// NUMBERS TO WORDS ///////////////////
-
-  // Write a condition(s) that checks the rollNum and write the string value of the number to the variable rollStr for output
-  let rollStr = ``
-
-  if (rollNum === 1) {
-    rollStr = `one`
-  } else if (rollNum === 2) {
-    rollStr = `two`
-  } else if (rollNum === 3) {
-    rollStr = `three`
-  } else if (rollNum === 4) {
-    rollStr = `four`
-  } else if (rollNum === 5) {
-    rollStr = `five`
-  } else if (rollNum === 6) {
-    rollStr = `six`
-  } else {
-    rollStr = `... I'm not sure, actually`
+  if (sum!== lastRoll)
+  {
+    document.querySelector("#number").textContent=` ${sum}`
   }
-
-
-  //////////////// CHECK IF DUPLICATE ///////////////////
-
-  // Check if this roll is the same as the last roll and say "again!" if so
-  let duplicateMsg = ``
-
-  // Assign the duplicate messaging
-  if (rollNum === lastRoll) {
-    duplicateMsg = ` again!`;
-  }
-
-  // Update the roll history for next time
-  lastRoll = rollNum;
-
-
-
-  /////////////////// UI UPDATES ////////////////////////
-
-  // Selecting UI elements
-  const eleMessage = document.querySelector(`#message`);
-  const eleDice = document.querySelector(`#dice`);
+  else 
+   {
+     console.log(sum+str)
+     document.querySelector("#number").textContent=(sum+str);
+    }
+   lastRoll=sum
+   if (sum ===2){document.querySelector(".message").textContent="You rolled : Snake Eyes"}
+   else if (sum ===3)   {document.querySelector(".message").textContent="You rolled : Ace Deuce"} 
+   else if (sum ===5)   {document.querySelector(".message").textContent="You rolled : Five (Fever Five)"} 
+   else if (sum ===7)   {document.querySelector(".message").textContent="You rolled : Natural/Seven Out"} 
+   else if (sum ===9)   {document.querySelector(".message").textContent="You rolled : Nine (Nina)"} 
+   else if (sum ===11)  {document.querySelector(".message").textContent="You rolled : Yo (Yo-leven)"} 
+   else if (sum ===12)  {document.querySelector(".message").textContent="You rolled : Boxcars/Midnight"} 
  
-  // Update the #message's textContent
-  eleMessage.textContent = `You rolled: ${rollStr}${duplicateMsg}`;
+   else if (sum ===4 && ((diceRoll1 ===3 && diceRoll2 ===1) || (diceRoll1 ===1 && diceRoll2 ===3)))  {document.querySelector(".message").textContent="You rolled : Easy Four"}
+   else if (sum ===4 && (diceRoll1 ===2 && diceRoll2 ===2))  {document.querySelector(".message").textContent="You rolled : Hard Four"} 
+ 
+   else if (sum ===6 && ((diceRoll1 ===2 && diceRoll2 ===4) || (diceRoll1 ===4 && diceRoll2 ===2))) {document.querySelector(".message").textContent="You rolled : Easy Six"}
+   else if (sum ===6 && ((diceRoll1 ===1 && diceRoll2 ===5) || (diceRoll1 ===5 && diceRoll2 ===1))) {document.querySelector(".message").textContent="You rolled : Easy Six"}
+   else if (sum ===6 && (diceRoll1 ===3 && diceRoll2 ===3))  {document.querySelector(".message").textContent="You rolled : Hard Six"} 
   
-  // Update the #dice's src attribute
-  eleDice.setAttribute(`src`, `img/dice${rollNum}.svg`);
-  // eleDice.src = `img/dice5.svg` // BAD PRACTICE
+   else if (sum ===8 && ((diceRoll1 ===6 && diceRoll2 ===2) || (diceRoll1 ===2 && diceRoll2 ===6)))  {document.querySelector(".message").textContent="You rolled : Easy Eight"}
+   else if (sum ===8 && ((diceRoll1 ===3 && diceRoll2 ===5) || (diceRoll1 ===5 && diceRoll2 ===3)))  {document.querySelector(".message").textContent="You rolled : Easy Eight"}
+   else if (sum ===8 && (diceRoll1 ===4 && diceRoll2 ===4))  {document.querySelector(".message").textContent="You rolled : Hard Eight"}
 
-  // Update the #dice's alt attribute
-  eleDice.setAttribute(`alt`, `Dice face ${rollNum}`);
+   else if (sum ===10 && ((diceRoll1 ===4 && diceRoll2 ===6) || (diceRoll1 ===6 && diceRoll2 ===4))) {document.querySelector(".message").textContent="You rolled : Easy Ten"}
+   else if (sum ===10 && (diceRoll1 ===5 && diceRoll2 ===5))  {document.querySelector(".message").textContent="You rolled : Hard Ten"} 
 
-
-
-  // Unnecessary, but always a good idea to return something
-  return rollNum;
+   document.querySelector(".one").setAttribute("src",`./img/dice${diceRoll1}.svg`)    
+   document.querySelector(".two").setAttribute("src",`./img/dice${diceRoll2}.svg`)    
+   return sum
 }
 
-
-// When the button is clicked, callback to rollTheDice (a function defined above)
-const eleRoll = document.querySelector(`#roll`)
-eleRoll.addEventListener(`click`, rollTheDice)
-
+document.querySelector("#btn").addEventListener(`click`,rollTheDice)
